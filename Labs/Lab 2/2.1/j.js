aac88b6d623b4386df6891b7dcd4e686b06a3663
IBinLakpa <sherchan.shreyash@gmail.com>
2022-12-30 07:46:34 +0545

    readme

--- Labs/Lab 2/2.1/j.js
+++ Labs/Lab 2/2.1/j.js
@@ -1,6 +1,27 @@
-
+window.onload = function() {
+    d = document.getElementById('screen_divider');
+    s_l = document.getElementById('s_left');
+    s_r = document.getElementById('s_right');
+    d.addEventListener('mousedown',shift_divider,false);
+    s_l.addEventListener('mouseup',move_left,false)
+    //d.addEventListener('click',move_div,false);
+}
 function shift_divider(){
-    const divider = document.getElementById('screen_divider');
+    const d = document.getElementById('screen_divider');
     let x="yellow";
-    divider.style.backgroundColor=x;
+    d.style.backgroundColor=x;
+}
+function move_div(){
+    const d = document.getElementById('screen_divider');
+    d.style.backgroundColor="blue";
+    let posX="70px";
+    d.style.left="70px";
+}
+function move_left(){
+    const d = document.getElementById('screen_divider');
+    const s_l = document.getElementById('s_left');
+    const s_r = document.getElementById('s_right');
+    d.style.left+="5%";
+    s_l.style.width-="5%";
+    s_r.style.width+="5%";
 }

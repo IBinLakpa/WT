5db6649a878be80dc3e88861b19860ca163d0295
IBinLakpa <sherchan.shreyash@gmail.com>
2023-01-20 08:54:02 +0545

    bug fix

--- Labs/Lab 4/4.2/j.js
+++ Labs/Lab 4/4.2/j.js
@@ -1,19 +1,26 @@
-let element, el;
-
-function stacking(class1, class2, class3,event){
-	document.getElementsByClassName(class1)[0].style.zIndex = "10";
-	document.getElementsByClassName(class2)[0].style.zIndex = "0";
-	document.getElementsByClassName(class3)[0].style.zIndex = "0";
-	grab(event,class1);			
+let element;
+const spanElements = document.querySelectorAll("div");
+for(el of spanElements) {
+	el.addEventListener("mousedown", grab, true)
 }
-function grab(event, class1){
-	document.addEventListener("mousemove", mover(event, class1,), true);
-	document.addEventListener("mouseup", droper(event), true);
-    console.log("grab")
+function grab(event){
+	element = event.currentTarget;
+	document.addEventListener("mousemove", mover, true);
+	document.addEventListener("mouseup", droper, true);
+	for(el of spanElements) {
+    	if(el.style.zIndex=='2'){
+	    	el.style.zIndex='1';
+	        console.log('set 1')
+    	}
+		else if(el.style.zIndex=='1'){
+    	   el.style.zIndex='0';
+    	}
+	}
+    element.style.zIndex='2';
 }
-function mover(event, class1){
-	document.getElementsByClassName(class1)[0].style.left = event.clientX + "px";
-	document.getElementsByClassName(class1)[0].style.top = event.clientY + "px";
+function mover(event){
+	element.style.left = event.clientX + "px";
+	element.style.top = event.clientY + "px";
 }
 function droper(event){
 	document.removeEventListener("mousemove", mover, true);

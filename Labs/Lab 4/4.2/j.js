2500ed7b9717f30f40f17fa4c63334c2b61795e4
IBinLakpa <sherchan.shreyash@gmail.com>
2023-01-20 08:14:03 +0545

    lab 4.2

--- Labs/Lab 4/4.2/j.js
+++ Labs/Lab 4/4.2/j.js
@@ -1,19 +1,19 @@
 let element, el;
-const spanElements = document.getElementsByTagName("div");
 
-for(el of spanElements) {
-	el.addEventListener("mousedown", grab, true)
-    console.log("set")
+function stacking(class1, class2, class3,event){
+	document.getElementsByClassName(class1)[0].style.zIndex = "10";
+	document.getElementsByClassName(class2)[0].style.zIndex = "0";
+	document.getElementsByClassName(class3)[0].style.zIndex = "0";
+	grab(event,class1);			
 }
-function grab(event,element){
-	element = event.currentTarget;
-	document.addEventListener("mousemove", mover(element), true);
-	document.addEventListener("mouseup", droper(element), true);
+function grab(event, class1){
+	document.addEventListener("mousemove", mover(event, class1,), true);
+	document.addEventListener("mouseup", droper(event), true);
     console.log("grab")
 }
-function mover(event, element){
-	element.style.left = event.clientX + "px";
-	element.style.top = event.clientY + "px";
+function mover(event, class1){
+	document.getElementsByClassName(class1)[0].style.left = event.clientX + "px";
+	document.getElementsByClassName(class1)[0].style.top = event.clientY + "px";
 }
 function droper(event){
 	document.removeEventListener("mousemove", mover, true);
